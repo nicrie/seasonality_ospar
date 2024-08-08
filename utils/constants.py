cd0ca306a7dd0ca99d37cf578ab30d63dce6bda2
Niclas Rieger <niclasrieger@gmail.com>
2024-08-08 12:14:44 +0200

    clean up all

--- utils/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-from string import ascii_uppercase as ABC
-
-# CATEGORIES as defined by OSPAR
-_path_categories = os.path.join(os.getcwd(), 'utils/categories.json')
-f = open(_path_categories)
-CATEGORIES = json.load(f)
